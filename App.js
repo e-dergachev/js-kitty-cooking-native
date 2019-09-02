07959735af87f149bfd636c6782998494ac99328
Evgenii Dergachev <edergachev@bk.ru>
2019-09-02 22:10:33 +1000

    fixing sqlite discrepancy between apk and test environment

--- App.js
+++ App.js
@@ -24,9 +24,21 @@ function App() {
 
   async function loadDB() {
     //await FileSystem.deleteAsync(`${FileSystem.documentDirectory}SQLite/recipes.sqlite3`);
-    const check = await FileSystem.getInfoAsync(`${FileSystem.documentDirectory}SQLite/recipes.sqlite3`);
+    /*const check = await FileSystem.getInfoAsync(`${FileSystem.documentDirectory}SQLite/recipes.sqlite3`);
     if (!check.exists) {
       await FileSystem.downloadAsync(Asset.fromModule(require('./assets/db/recipes.sqlite3')).uri, `${FileSystem.documentDirectory}SQLite/recipes.sqlite3`);
+    }*/
+    try {
+      await FileSystem.makeDirectoryAsync(`${FileSystem.documentDirectory}SQLite`, {
+        intermediates: true
+      });
+      const check = await FileSystem.getInfoAsync(`${FileSystem.documentDirectory}SQLite/recipes.sqlite3`);
+      if (!check.exists) {
+        await FileSystem.downloadAsync(Asset.fromModule(require('./assets/db/recipes.sqlite3')).uri, `${FileSystem.documentDirectory}SQLite/recipes.sqlite3`);
+      }
+    }
+    catch (error) {
+      console.log(error);
     }
   }
 
